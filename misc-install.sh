2dcdaee1efa6105620ce54f97433b58ddbb3e899
Konstantin Groshev <real.mt@gmail.com>
2022-02-15 15:12:46 +0300

    [+] install spaceship theme for zsh

---
+++ misc-install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This installs the spaceship theme for zsh
+# https://github.com/denysdovhan/spaceship-prompt
+if [ -d "$ZSH/custom/themes/spaceship-prompt" ]
+then
+    echo "spaceship-prompt is already installed, skipping..."
+else
+    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH/custom/themes/spaceship-prompt"
+    ln -s "$ZSH/custom/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH/custom/themes/spaceship.zsh-theme"
+fi
